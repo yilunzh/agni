8f5baee1ba6f15d51fad199f26edd70cfc5a554c
Yilun Zhang <yilunzh@Yiluns-MacBook-Pro.local>
2015-06-13 21:47:31 -0700

    updated display

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -7,30 +7,43 @@ class SearchController < ApplicationController
 		if params['/search'].nil?
 			@styles = []
 		else
+			clean_search_params
 			@styles = Style.joins(:modelyear).where("submodel -> 'body' ilike '%#{params['/search'][:bodytype]}%'  
 																							 and modelyears.year >= #{params['/search'][:minyear].to_i}
 																							 and modelyears.year <= #{params['/search'][:maxyear].to_i}")
 										 .joins(:consumer_ratings)
 										 .order("consumer_ratings.averagerating DESC, consumer_ratings.reviewscount DESC").limit(4)
-			@styles.each do |style|
-				result = {}
-				modelyear = style.modelyear
-				make = modelyear.make
-				media = style.media.where("media.shot_type_abbr = 'FQ' and media.category = 'EXTERIOR'").first
-				photo = ""
-				media["sources"].each do |source|
-					if source["size"]["width"] == 300
-						photo = "http://media.ed.edmunds-media.com" + source["link"]["href"]
+			unless @styles.empty? 
+				@styles.each do |style|
+					result = {}
+					modelyear = style.modelyear
+					make = modelyear.make
+					media = style.media.where("media.shot_type_abbr = 'FQ' and media.category = 'EXTERIOR'").first
+					binding.pry
+					photo = ""
+					if media
+						media["sources"].each do |source|
+							if source["size"]["width"] == 300
+								photo = "http://media.ed.edmunds-media.com" + source["link"]["href"]
+							end
+						end
 					end
-				end
-				if not style.consumer_ratings.empty?
-					rating = style.consumer_ratings[0]["averagerating"]
-					reviews_count = style.consumer_ratings[0]["reviewscount"]
-				end
+					if not style.consumer_ratings.empty?
+						rating = style.consumer_ratings[0]["averagerating"]
+						reviews_count = style.consumer_ratings[0]["reviewscount"]
+					end
 
-				result = { make: make, modelyear: modelyear, style: style, rating: rating, reviews_count: reviews_count, photo: photo}
-				@results << result
+					result = { make: make, modelyear: modelyear, style: style, rating: rating, reviews_count: reviews_count, photo: photo}
+					@results << result
+				end
 			end
 		end
+	end
+
+	private
+
+	def clean_search_params
+		params["/search"][:minyear] = 1990 if params['/search'][:minyear].empty?
+		params["/search"][:maxyear] = Time.now.year if params['/search'][:maxyear].empty?
 	end
 end
